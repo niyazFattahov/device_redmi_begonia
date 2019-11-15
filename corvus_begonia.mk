5b0765c2bdbf001bc3533c0c5df1631913106a6d
Justin Yun <justinyun@google.com>
2019-11-15 16:08:00 +0900

    begonia: Inherit base system_ext.mk files for aosp targets
    Make aosp targets inherit handheld_system_ext.mk and
    telephony_system_ext.mk files.
    Devices that have /system_ext or /system/system_ext must inherit any
    of *_system_ext.mk files to install mandatory packages for system_ext
    partition.
    
    Bug: 144542478
    Test: Build aosp targets
    Change-Id: Ie160998150caa953c9a34726525cdbcefc222841
    Merged-In: Ie160998150caa953c9a34726525cdbcefc222841
    (cherry picked from commit 3464451392891c13d458c6a48a7f66b7e234959a)

--- corvus_begonia.mk
+++ corvus_begonia.mk
@@ -22,6 +22,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
 # Inherit from begonia device
 $(call inherit-product, device/redmi/begonia/device.mk)
 

e39cc181ccab2728b12cf5b8a7faf23230028557
Flamefusion <meshekharbehera613@gmail.com>
2020-09-16 19:38:28 +0000

    begonia: Fingerprint updated to Coral September fingerprint

--- du_begonia.mk
+++ du_begonia.mk
@@ -35,7 +35,7 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="begonia-user 10 QP1A.190711.020 V11.0.2.0.QGGEUXM release-keys" \

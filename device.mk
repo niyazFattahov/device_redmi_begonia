85d3303fae5c0f79a8b3dda10b2e4e7fca05e4e2
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    begonia: Build libhidltransport/libhwbinder From Source
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device.mk
+++ device.mk
@@ -32,6 +32,13 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0_system
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
